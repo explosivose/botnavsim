4662608618dd6c5b67fbcc05ce4cce9bf540e826
mattblickem <explosivose@gmail.com>
2014-12-15 17:10:37 +0000

    moved some files around

--- Assets/botnavsim/Scripts/UI/Strings.cs
+++ Assets/botnavsim/Scripts/UI/Strings.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Strings  {
+public static class Strings  {
 
+	public const string projectTitle = "BotNavSim";
+	public const string projectVersion = "v0.2.0";
+	
+	
 	public static bool IsDigitsOnly(string str) {
 		foreach (char c in str) {
 			if (c < '0' || c > '9') {
@@ -11,4 +15,5 @@ public class Strings  {
 		}
 		return true;
 	}
+	
 }
