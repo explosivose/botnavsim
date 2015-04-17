15a8bc5a16e1305e5bc78a5e5c5bd27ded7687d5
Matt Blickem <matt@supercore.co.uk>
2015-04-17 15:32:19 +0100

    moved pid and propeller classes out of ardrone class

--- Assets/botnavsim/Scripts/Robots/ArDrone.cs
+++ Assets/botnavsim/Scripts/Robots/ArDrone.cs
@@ -3,36 +3,6 @@ using System.Collections;
 
 [RequireComponent(typeof(Robot))]
 public class ArDrone : MonoBehaviour {
-
-	[System.Serializable]
-	public class Pid {
-		public float kp, ki, kd;
-		public float error {get; private set;}
-		public float previous_error {get; private set;}
-		public float integral {get; private set;}
-		public float derivative {get; private set;}
-		
-		public float output(float target, float actual) {
-			error = target - actual;
-			integral += error * Time.fixedDeltaTime;
-			derivative = (error - previous_error) / Time.fixedDeltaTime;
-			previous_error = error;
-			return (kp*error) + (ki*integral) + (kd*derivative);
-		}
-		
-		public void CopySettings(Pid pid) {
-			kp = pid.kp;
-			ki = pid.ki;
-			kd = pid.kd;
-		}
-	}
-	
-	[System.Serializable]
-	public class Propeller {
-		public Transform tr;
-		public Pid controller;
-		
-	}
 	
 	public float forwardThrust = 2f;
 	public Pid verticalController;
---
+++ Assets/botnavsim/Scripts/Robots/Propeller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Propeller model. Fields are exposed to Unity Inspector
+/// </summary>
+[System.Serializable]
+public class Propeller {
+	public Transform tr;
+	public Rigidbody rb;
+	public float maxThrust;
+	public Pid controller;
+}
